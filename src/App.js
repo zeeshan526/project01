508900605bfeadb9893fca58e27b4ca1c8e9ca8d
zeeshan526 <55299089+zeeshan526@users.noreply.github.com>
2020-06-12 11:55:19 +0500

    update-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Room from './Room';
 
 function App() {
+    //const state = useState(true);
+    //console.log("State=",state);
+    const[Count, setCount] =useState(1);
+
+//    function updateLit(){
+//        console.log("button clicked")
+//    }
+    const [isMorning, setMorning]=useState(true);
+
   return (
-    <div>
-Successfully Deploying A React Project!!!!
+    <div className='box'>
+
+    <h1>Have a good {isMorning?'Morning':'Night'}</h1>
+      <Room Counter={Count}/>
+      <br/>
+      <button onClick={()=> setCount(Count+1)}>update state</button>
+      <button onClick={()=>setMorning(!isMorning)}>update day</button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
---
+++ src/Room.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+
+function Room(props) {
+  return (
+    <div>
+      value of variable is :{props.Counter}
+    </div>
+  );
+}
+
+export default Room;
